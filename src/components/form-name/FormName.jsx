89daab6aff56130f68094bfa48aadf74e342c71f
dino-ctk <dduvnjak@ctk-rijeka.hr>
2025-05-28 11:46:10 +0200

    spread operator

--- src/components/form-name/FormName.jsx
+++ src/components/form-name/FormName.jsx
@@ -3,33 +3,57 @@ import { use } from "react";
 
 function FormName(){
 
+    const cirtusi = ["limun", "grejp", "mandarina"]
+    const voce = ["banana", ...cirtusi, "jabuka", "mango"]
+
+    // console.log(voce);
+
+    const cijeloName = {
+        firstName: "dino",
+        lastName: "Duvnjak"
+    }
+
+    const user = {
+        ...cijeloName,
+        id:1,
+        username: "bongo"
+    }
+
+
+
+
+
     const [fullName, setFullName] = useState({
         fName: "",
         lName: ""
     })
 
-    function handleOnChange(event){
-        console.log(event.target.value);
 
-        console.log(event);
+    function handleOnChange(event){
+        console.log(event.target);
+
+        const {name, value, type} = event.target;
+
+        console.log(name);
+        console.log(value);
+        console.log(type);
+
+
+        const [red, green, blue] = ["browsVlue", "greenValue", "blueValue"]
+
+        console.log(red)
+        console.log(green)
 
         setFullName((previous) => {
-            if(event.target.name === "fName"){
-                return{
-                    fName: event.target.value,
-                    lName: previous.lName
-                }
-            
-            }else if (event.target.name === "lName"){
-                return{
-                    fName: previous.fName,
-                    lName: event.target.value
-                }
+            console.log("previous je: ", previous)
+            console.log("current je :", value);
+            var noveVrijednosti = {
+                ...previous,
+                [name]: value
             }
-           
+           // console.log("nove val: ", noveVrijednosti)
+            return noveVrijednosti;
         })
-
-        console.log("fullname je: ", fullName)
     }
 
     return(
