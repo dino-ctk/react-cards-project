656a160b8fb34108577a6b75aa58c36124c8748a
dino-ctk <dduvnjak@ctk-rijeka.hr>
2025-05-28 10:20:00 +0200

    complex states

--- src/components/form-name/FormName.jsx
+++ src/components/form-name/FormName.jsx
@@ -1,27 +1,42 @@
-import React, {useState} from "react";
+import React, {useRef, useState} from "react";
+import { use } from "react";
 
 function FormName(){
 
-    const [firstName, setFirstName] = useState("")
-    const [lastName, setLastName] = useState("")
-
+    const [fullName, setFullName] = useState({
+        fName: "",
+        lName: ""
+    })
 
     function handleOnChange(event){
         console.log(event.target.value);
-        if(event.target.name === "fName"){
-            setFirstName(event.target.value)            
-        }else if (event.target.name === "lName"){
-            setLastName(event.target.value)
-        }
-        
-        //setFirstName("adasd");
+
+        console.log(event);
+
+        setFullName((previous) => {
+            if(event.target.name === "fName"){
+                return{
+                    fName: event.target.value,
+                    lName: previous.lName
+                }
+            
+            }else if (event.target.name === "lName"){
+                return{
+                    fName: previous.fName,
+                    lName: event.target.value
+                }
+            }
+           
+        })
+
+        console.log("fullname je: ", fullName)
     }
 
     return(
         <div>
-            <h1>Hello: {firstName} {lastName}</h1>
-            <input name="fName" type="text" value={firstName} onChange={handleOnChange} />
-            <input name="lName" type="text" value={lastName} onChange={handleOnChange} />
+            <h1>Hello: {fullName.fName} {fullName.lName}</h1>
+            <input name="fName"  type="text" value={fullName.fName} onChange={handleOnChange} />
+            <input name="lName" type="text" value={fullName.lName} onChange={handleOnChange} />
         </div>
     )
 
